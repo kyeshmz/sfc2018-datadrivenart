1e03bebb43620ce8734bde51ba19e7ac8aff5a20
kkshmz <kyeshimizu@gmail.com>
2018-07-29 23:54:16 +0900

    Initial Commit

---
+++ dda_openBCI16/src/ofApp.h
@@ -0,0 +1,36 @@
+#pragma once
+
+#include "ofMain.h"
+#include "ofxOpenBCI.hpp"
+#include "ofxGui.h"
+
+// Add the OpenBCI OSC Address here
+//listening on port
+
+#define PORT 12345
+#define SEND_PORT 8887
+
+class ofApp : public ofBaseApp{
+
+	public:
+		void setup();
+		void update();
+		void draw();
+
+		void keyPressed(int key);
+		void keyReleased(int key);
+		void mouseMoved(int x, int y );
+		void mouseDragged(int x, int y, int button);
+		void mousePressed(int x, int y, int button);
+		void mouseReleased(int x, int y, int button);
+		void mouseEntered(int x, int y);
+		void mouseExited(int x, int y);
+		void windowResized(int w, int h);
+		void dragEvent(ofDragInfo dragInfo);
+		void gotMessage(ofMessage msg);
+    
+    ofxOpenBCIReceiver receiver;
+    ofxPanel gui;
+    ofxToggle log, add, smooth, range, send, printV;
+		
+};
